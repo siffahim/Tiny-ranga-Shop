6c1b02490d492489e197d5e39804d110ff72a00e
siffahim <siffahim25@gmail.com>
2021-09-13 08:46:20 +0600

    fixed bug

--- js/app.js
+++ js/app.js
@@ -35,6 +35,7 @@ const addToCart = (id, price) => {
   updatePrice("price", price);
   updateTaxAndCharge();
   updateTotal()
+
   document.getElementById("total-Products").innerText = count;
 };
 
@@ -66,7 +67,7 @@ const detail = async id => {
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -75,7 +76,7 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = total.toFixed(2);
+  document.getElementById(id).innerText = parseFloat(total.toFixed(2));
 };
 
 // set innerText function
@@ -98,6 +99,7 @@ const updateTaxAndCharge = () => {
     setInnerText("delivery-charge", 60);
     setInnerText("total-tax", priceConverted * 0.4);
   }
+
 };
 
 //grandTotal update function
@@ -106,6 +108,6 @@ const updateTotal = () => {
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
   
-  document.getElementById("total").innerText = grandTotal;
+  document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
 updateTotal()
